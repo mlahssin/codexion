216a2e618082be9f4dbb68d319cc69ae936da0ff
Moaad Lahssini <mlahssin@e2r5p11.1337.ma>
2026-07-08 10:19:34 +0100

    second move

--- src1/main.c
+++ src1/main.c
@@ -4,26 +4,41 @@ int is_available(t_dongle   dongle)
 {
     return  dongle.used == -1;
 }
+void    dongle_init(t_dongle    *dongles, t_params  params)
+{
+    for (int i = 0; i < params.num_coders; i++)
+    {
+        dongles[i].used = -1;
+        pthread_mutex_init(&dongles[i].dongle_mutex, NULL);
+    }
+}
 
+void    coders_init(t_coder *coders, t_params    params, t_sim  *sim)
+{
+    for (int i = 0; i < params.num_coders; i++)
+    {
+        coders[i].id = i;
+        coders[i].compile_count = 0;
+        coders[i].right_dongle = NULL;
+        coders[i].left_dongle = NULL;
+        coders[i].last_compile_start = 0;
+        coders[i].sim = sim;
+    }
+
+}
 void    *simulation(void    *arg)
 {
     t_coder *coder = (t_coder   *)arg;
-    // while (i < params.num_coders)
-    // {
     if (coder->id % 2 == 0)
     {
         pthread_mutex_lock(&coder->sim->dongles[coder->id].dongle_mutex);
         if (is_available(coder->sim->dongles[coder->id]))
         {
-        //     pthread_mutex_lock(&coder->sim->dongles[coder->id].dongle_mutex);
             coder->left_dongle = &coder->sim->dongles[coder->id];
             printf("coder %d takes dongle : %d\n", coder->id, coder->id);
-            coder->sim->dongles[coder->id].used = coder->id;
-            // pthread_mutex_unlock(&coder->sim->dongles[coder->id].dongle_mutex);
-   
+            coder->sim->dongles[coder->id].used = coder->id;   
         }
         pthread_mutex_unlock(&coder->sim->dongles[coder->id].dongle_mutex);
-
     }
     else
     { 
@@ -36,7 +51,6 @@ void    *simulation(void    *arg)
                 coder->right_dongle = &coder->sim->dongles[coder->id + 1];
                 printf("coder %d takes dongle : %d\n", coder->id, coder->id + 1);
                 coder->sim->dongles[coder->id + 1].used = coder->id;
-                
             }
             pthread_mutex_unlock(&coder->sim->dongles[coder->id + 1].dongle_mutex);
         }
@@ -56,9 +70,6 @@ void    *simulation(void    *arg)
         }
     }
     return NULL;
-        // coder->id++;
-    // }
-
 }
 
 
@@ -76,29 +87,18 @@ int main(int ac, char *av[])
     pthread_t    coder_ths[params.num_coders];
     sim.params = params;
     t_coder coders[params.num_coders];
+    
 
     t_dongle   dongles[params.num_coders];
-    for (int i = 0; i < params.num_coders; i++)
-    {
-        coders[i].id = i;
-        coders[i].compile_count = 0;
-    }
 
-    for (int i = 0; i < params.num_coders; i++)
-    {
-        dongles[i].used = -1;
-        pthread_mutex_init(&dongles[i].dongle_mutex, NULL);
-    }
+    dongle_init(dongles, params);
+
     sim.dongles = dongles;
-    for (int i = 0; i < params.num_coders; i++)
-    {
-        coders[i].sim = &sim;
-    }
+    coders_init(coders, params, &sim);
     
     for (int i = 0; i < params.num_coders; i++)
     {
         pthread_create(coder_ths + i, NULL, simulation, (void *)&coders[i]);
-
     }
 
     for (int i = 0; i < params.num_coders; i++)
